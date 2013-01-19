d8b93b5e18aace62077dd940f354265c93d1275f
Ricardo Tomasi <ricardobeat@gmail.com>
2013-01-19 20:29:01 -0200

    update tests

---
+++ test/spec.js
@@ -0,0 +1,31 @@
+var test = require('tape')
+  , DOM = require('../rye.dom')
+
+test('get text', function(t){
+    var div = document.createElement('div')
+    div.textContent = 'hello'
+    t.equal(DOM.text(div), 'hello')
+    t.end()
+})
+
+test('set text', function(t){
+    var div = document.createElement('div')
+    DOM.text(div, 'hello')
+    t.equal(div.textContent, 'hello')
+    t.end()
+})
+
+test('get html', function(t){
+    var div = document.createElement('div')
+    div.innerHTML = '<p>hello</p>'
+    t.equal(DOM.html(div), div.innerHTML)
+    t.end()
+})
+
+test('set html', function(t){
+    var div = document.createElement('div')
+      , html = '<p>he<b>l</b>lo</p>'
+    DOM.html(div, html)
+    t.equal(div.innerHTML, html)
+    t.end()
+})
